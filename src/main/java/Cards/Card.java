e074a1160d9c988fe9c248f249a903161174d83f
Ezekiel A. Mitchell <ezekielamitchell@outlook.com>
2022-06-30 16:29:09 -0700

    small update

--- src/main/java/Cards/Card.java
+++ src/main/java/Cards/Card.java
@@ -5,14 +5,14 @@ import java.util.Random;
 
 public class Card {
 
-    private String card;
+    private final String card;
 
     String[] number = {"Ace", "King", "Queen", "Jack",
             "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
 
     String[] kind = {"Spades", "Hearts", "Diamonds", "Clubs"};
 
-    public void setCard() {
+    public Card() {
         Random random = new Random();
         card = (number[random.nextInt(number.length)] +
                 " of " + kind[random.nextInt(kind.length)]);
--- src/main/java/Cards/Game.java
+++ src/main/java/Cards/Game.java
@@ -1,9 +1,11 @@
 package Cards;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Game {
     public static void main(String[] args) {
         Card card = new Card();
-        card.setCard();
 
         System.out.println("Guess the card!");
         System.out.println("use notation: 'value' of 'kind'\ne.g. 'Ace of Spades'");
@@ -14,5 +16,7 @@ public class Game {
             System.out.println("Try again: ");
             userGuess.setUserGuess();
         }
+
+        System.out.println("Correct guess!");
     }
 }
